0cd77786be4dda54713520991c5166318df9f051
Touhid Mia <hmtm.cse@gmail.com>
2017-11-19 21:17:10 +0600

    menu listing

--- src/View/Helper/MenuHelper.php
+++ src/View/Helper/MenuHelper.php
@@ -14,20 +14,21 @@ use Cake\View\Helper;
 class MenuHelper extends Helper
 {
 
-    public $helpers = ['Html'];
+    public $helpers = ['Html','Url'];
 
     public function getMenu()
     {
         $fileAndDirectoryService = new FileAndDirectoryService();
         $menuList = $fileAndDirectoryService->getMenuList();
         $html = '';
+        $url = $this->Url->build('/assets/images');
         foreach ($menuList as $menu){
 //            print_r($menu);
             $html .= '<a href="#">';
             $html .= '<div class="content-block media">';
             $html .= '<div class="media-body text-center">';
 //            $html .= $this->Html->image('default_thumbs.jpg', ['alt' => 'Default', 'class' => 'img-thumbnail']);
-            $html .= '<img src= "/app/images/get" class="img-thumbnail" />';
+            $html .= '<img src= "' . $url .'" class="img-thumbnail" />';
             $html .= '<h5>' . $menu->displayName .'</h5>';
             $html .= '</div></div></a>';
         }
